a8ed64eea650e68f445d2016b2b21dabd9061c34
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-16 19:32:26 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,10 +10,10 @@ class InteractiveRecord
   def self.column_names
     DB[:conn].results_as_hash = true
 
-    sql = "PRAGMA table_info('#{table_name}')"
+      sql = "PRAGMA table_info('#{table_name}')"
 
-    table_info = DB[:conn].execute(sql)
-    column_names = []
+      table_info = DB[:conn].execute(sql)
+      column_names = []
 
     table_info.each do |column|
       column_names << column["name"]
@@ -27,12 +27,12 @@ class InteractiveRecord
     end
   end
 
-  def table_name_for_insert
-    self.class.table_name
-  end
-
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def table_name_for_insert
+    self.class.table_name
   end
 
   def values_for_insert
